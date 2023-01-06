9edef6c8229ff1b9128b708621beecb0cb135da5
frank.wong <frank.wong@ruoshitech.com>
2023-01-06 18:29:23 +0800

    添加 功能

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # picgo-plugin-moon
-An uploader for moon.
+
+为 [PicGo](https://github.com/Molunerfinn/PicGo) 开发的一款插件，新增了moon玄兔图床。
+该上传插件将图片上传到用户的素材管理中心。填写`Cookie`即可，获取方式在下面。
+
+## 目录
+1. [安装](#安装)
+2. [获取API和Cookie](#获取API和Cookie)
+3. [图片样式](#图片样式)
+4. [解决防盗链](#解决防盗链)
+
+## 安装
+
+- 在线安装
+
+  打开 [PicGo](https://github.com/Molunerfinn/PicGo) 详细窗口，选择**插件设置**，搜索**picgo-plugin-moon**安装，然后重启应用即可。
+
+- 离线安装
+
+  克隆该项目，复制项目到 以下目录：
+    - Windows: `%APPDATA%\picgo\`
+    - Linux: `$XDG_CONFIG_HOME/picgo/` or `~/.config/picgo/`
+    - macOS: `~/Library/Application\ Support/picgo/`
+
+  切换到新目录执行 `npm install ./picgo-plugin-moon`，然后重启应用即可。
+
+
+## 获取API和Cookie
+
+1. [获取API和Cookie](https://zhuanlan.zhihu.com/p/597017126)
+
+## 图片样式
+
+例如原图： <a href="http://example.com/0" rel="noreferrer" target=”_blank“>http://example.com/0</a>
+
+| Type  | Url     | 
+| ------| --------|
+| 原图压缩WebP  | baseURL/0  |
+| 原有体积2倍？可能压缩宽高不大于640  | baseURL/640  |
+| 原图压缩WebP？可能压缩宽高不大于640  | baseURL/641  |
+| 原有体积2倍？可能压缩宽高不大于1000  | baseURL/1000  |
+
+
+## 解决防盗链
+
+>通常网站开启防盗链，利用的是HTTP的Referer属性做判断。
+
+#### 站点页面使用
+
+HTML的Head区中定义如下元信息。
+
+```html
+<meta name="referrer" content="no-referrer">
+```
+
+#### 新窗口打开使用
+
+A标签中属性设置`rel="noreferrer"`。
+
+```html
+<a rel="noreferrer" target="_blank"></a> 
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,118 @@
+const fs   = require('fs')
+const path = require('path')
+
+module.exports = (ctx) => {
+    const register = () => {
+        ctx.helper.uploader.register('moon-txom', {
+            handle,
+            name: 'moon-txom玄兔图床',
+            config: config
+        })
+    }
+
+    const postOptions = (args) => {
+        return {
+            method: 'POST',
+            url: args.api,
+            headers: {
+                contentType: 'multipart/form-data',
+                Cookie: args.cookie
+            },
+            formData: {
+                file: args.image,
+                Filename: args.fileName
+            }
+        }
+    }
+
+    const handle = async (ctx) => {
+        let userConfig = ctx.getConfig('picBed.moon-txom')
+        if (!userConfig.api) {
+            ctx.emit('notification', {
+                title: '请配置API地址',
+                body: '打开浏览器查看API地址',
+                text: 'https://zhuanlan.zhihu.com/p/597017126'
+            })
+            return
+        }
+        if (!userConfig.cookie) {
+            ctx.emit('notification', {
+                title: '请配置cookie',
+                body: '打开浏览器查看cookie的获取',
+                text: 'https://zhuanlan.zhihu.com/p/597017126'
+            })
+            return
+        }
+        const cookie = userConfig.cookie
+        const imgList = ctx.output
+        for (let i in imgList) {
+            let image = imgList[i].buffer
+            if (!image && imgList[i].base64Image) {
+                image = Buffer.from(imgList[i].base64Image, 'base64')
+            }
+            const data     = new Uint8Array(image)
+            const fileName = imgList[i].fileName
+            const filePath = path.join(__dirname, fileName)
+
+            await fs.writeFileSync(filePath, data)
+
+            let args      = {}
+            args.api      = api
+            args.cookie   = cookie
+            args.fileName = fileName
+            args.image    = fs.createReadStream(filePath)
+
+            const postConfig = postOptions(args)
+            let body         = await ctx.Request.request(postConfig)
+            fs.unlink(filePath, () => {})
+            body = JSON.parse(body)
+            if (body.response.code === 0) {
+                delete imgList[i].base64Image
+                delete imgList[i].buffer
+
+                let url           = body.data.url.url.replace('http', 'https')
+
+                imgList[i].imgUrl = url+"/640"
+                imgList[i].url    = url+"/0"
+            } else {
+                ctx.emit('notification', {
+                    title: '上传失败',
+                    body: body.response.msg
+                })
+                throw new Error(body.response.msg)
+            }
+        }
+        return ctx
+    }
+
+    const config = ctx => {
+        let userConfig = ctx.getConfig('picBed.moon-txom')
+        if (!userConfig) {
+            userConfig = {}
+        }
+        return [
+            {
+                name: 'api',
+                type: 'input',
+                default: userConfig.api,
+                required: true,
+                message: 'api',
+                alias: 'api'
+            },
+            {
+                name: 'cookie',
+                type: 'input',
+                default: userConfig.cookie,
+                required: true,
+                message: 'cookie',
+                alias: 'cookie'
+            }
+        ]
+    }
+
+    return {
+        uploader: 'moon-txom',
+        config: config,
+        register
+    }
+}
